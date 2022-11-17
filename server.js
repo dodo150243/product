67271686765c02fdd06bf5749eb4af195730ca8d
TK DO <80673213+dodo150243@users.noreply.github.com>
2022-11-17 15:59:18 +0700

    add

--- server.js
+++ server.js
@@ -9,13 +9,72 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 
+const makeCrud = require('express-json-file-crud').makeCrud;
+const productCrud = makeCrud('order', './storage');
+app.use('/order', productCrud);
+
+//  database json
+const fs = require('fs');
+let rawdata = fs.readFileSync('db.json');
+let order = JSON.parse(rawdata);
+// console.log(order);
+
+// let data = [
+//     {
+//         "id_order": "IN1136",
+//         "done_on": null,
+//         "id": 0
+//     },
+//     {
+//         "id_order": "IN1136",
+//         "done_on": null,
+//         "id": 1
+//     },
+//     {
+//         "id_order": "IN1136",
+//         "done_on": "2022-08-27 10:49:59",
+//         "id": 2
+//     }
+// ];
+  
+
 
 app.get('/user', function(req, res, next) {
+
+    
     res.sendfile(__dirname+'/user.html');
+    io.on('connection',(socket) =>{
+        console.log(socket.id);
+        console.log('user socket connected')
+
+
+        socket.on('product', (response)=>{
+            console.log(response)
+            io.sockets.emit('pre_list', response)
+        })
+
+    })
  });
 
+//  const picked = (({ id, id_order, done_on }) => ({  id, id_order, done_on }))(employees[0]);
+
+
+ 
  app.get('/admin', function(req, res, next) {
+  
     res.sendfile(__dirname+'/admin.html');
+    res.json(console.log(order))
+    io.on('connection',(socket) =>{
+        console.log(socket.id);
+        console.log('admin socket connected')
+    
+        socket.on('product', (response)=>{
+             console.log(response)
+            io.sockets.emit('pre_list', response)
+            
+    
+        })
+    })
  });
 
 const server = app.listen(3000, ()=>{
@@ -23,12 +82,12 @@ const server = app.listen(3000, ()=>{
 })
 
 const io = socketIO(server)
-io.on('connection',(socket) =>{
-    console.log('client socket connected')
+// io.on('connection',(socket) =>{
+//     console.log('client socket connected')
 
-    socket.on('product', (response)=>{
-        console.log(response)
+//     socket.on('product', (response)=>{
+//         console.log(response)
 
-        io.sockets.emit('pre_list', response)
-    })
-})
+//         io.sockets.emit('pre_list', response)
+//     })
+// })
